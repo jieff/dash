999ea27ae0d3bd0997b072edbb64cc5c1e275258
Jieff Cavalcanti <jieff.cavalcanti@hotmail.com>
2022-03-10 19:35:01 -0300

    organizar rotas de admin

--- routes/web.php
+++ routes/web.php
@@ -5,28 +5,22 @@ use App\Http\Controllers\Admin\{
     PlanController
 };
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+Route::prefix('admin')->group(function(){
+
+    Route::post('plans/search', [PlanController::class, 'search'])->name('plans.search');
+    Route::get('plans/create', [PlanController::class, 'create'])->name('plans.create');
+    Route::put('plans/{url}', [PlanController::class, 'update'])->name('plans.update');
+    Route::get('plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
+    Route::delete('plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
+    Route::get('plans/{url}', [PlanController::class, 'show'])->name('plans.show');
+    Route::post('plans', [PlanController::class,'store'])->name('plans.store');
+    Route::get('plans', [PlanController::class, 'index'])->name('plans.index');
+    Route::get('/', [PlanController::class, 'index'])->name('admin.index');
 
 
-Route::post('admin/plans/search', [PlanController::class, 'search'])->name('plans.search');
-Route::get('admin/plans/create', [PlanController::class, 'create'])->name('plans.create');
-Route::put('admin/plans/{url}', [PlanController::class, 'update'])->name('plans.update');
-Route::get('admin/plans/{url}/edit', [PlanController::class, 'edit'])->name('plans.edit');
-Route::delete('admin/plans/{url}', [PlanController::class, 'destroy'])->name('plans.destroy');
-Route::get('admin/plans/{url}', [PlanController::class, 'show'])->name('plans.show');
-Route::post('admin/plans', [PlanController::class,'store'])->name('plans.store');
-Route::get('admin/plans', [PlanController::class, 'index'])->name('plans.index');
+});
 
-Route::get('admin', [PlanController::class, 'index'])->name('admin.index');
 
 Route::get('/', function () {
     return view('welcome');
